304b0ba25c398c5bdf8138b70c0b95b82d4e3a87
shriphani <spalakod@cs.cmu.edu>
2014-03-02 15:12:35 -0500

    initial commit made

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to detect_leaf
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/detect_leaf/core_test.clj
@@ -0,0 +1,7 @@
+(ns detect-leaf.core-test
+  (:use clojure.test
+        detect-leaf.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
