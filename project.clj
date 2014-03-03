e6c986f125f7bae7267a048b83134cb8f8e78550
shriphani <spalakod@cs.cmu.edu>
2014-03-02 19:21:35 -0500

    crawling and corpus building done

--- project.clj
+++ project.clj
@@ -3,4 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[clj-http "0.9.0"]
+                 [clj-logging-config "1.9.7"]
+                 [com.github.kyleburton/clj-xpath "1.4.2"]
+                 [log4j/log4j "1.2.16"
+                  :exclusions
+                  [javax.mail/mail
+                   javax.jms/jms
+                   com.sun.jdmk/jmxtools
+                   com.sun.jmx/jmxri]]
+                 [net.sourceforge.htmlcleaner/htmlcleaner "2.6"]
+                 [org.apache.commons/commons-lang3 "3.1"]
+                 [org.bovinegenius/exploding-fish "0.3.3"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/clojure "1.5.1"]])
--- src/detect_leaf/core.clj
+++ src/detect_leaf/core.clj
@@ -1,6 +1,95 @@
-(ns detect-leaf.core)
+(ns detect-leaf.core
+  (:require [detect-leaf.utils :as utils]
+            [org.bovinegenius [exploding-fish :as uri]])
+  (:use [clj-xpath.core :only [$x $x:node $x:node+ $x:text+]])
+  (:import (org.htmlcleaner HtmlCleaner DomSerializer CleanerProperties)
+           (org.w3c.dom Document)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn random-sample
+  [url-queue visited extract stop? corpus]
+  (if (or (empty? url-queue)
+          (stop? (count visited)))
+    corpus
+    (do (Thread/sleep 1000)
+        (let [tok (rand-int 2)
+              
+              picked-url (if (zero? tok) (first url-queue) (last url-queue))
+              body-queue-new (if (zero? tok)
+                               (rest url-queue)
+                               (drop-last url-queue))
+              
+              body (try (utils/download-with-cookie (:url picked-url))
+                        (catch Exception e nil))
+              updated-corpus (merge corpus {(:url picked-url)
+                                            body})
+              extracted-links (and
+                               body
+                               (extract body picked-url (clojure.set/union
+                                                         (set
+                                                          (map :url url-queue))
+                                                         (set visited))))]
+          (utils/sayln :url (:url picked-url))
+          (utils/sayln :visited (count visited))
+          (utils/sayln :left (count url-queue))
+          (utils/sayln :extracted (count extracted-links))
+        
+          (recur (concat body-queue-new
+                         (map
+                          (fn [x] {:url x})
+                          extracted-links))
+                 (conj visited (:url picked-url))
+                 extract
+                 stop?
+                 updated-corpus)))))
+
+(defn process-page
+  [page-src]
+  (let [cleaner (new HtmlCleaner)
+        props   (.getProperties cleaner)
+        _       (.setPruneTags props "script, style")
+        _       (.setOmitComments props true)]
+    (.clean cleaner page-src)))
+
+(defn html->xml-doc
+  "Take the html and produce an xml version"
+  [page-src]
+  (let [tag-node       (-> page-src
+                           process-page)
+
+        cleaner-props  (new CleanerProperties)
+
+        dom-serializer (new DomSerializer cleaner-props)]
+    
+    (-> dom-serializer
+        (.createDOM tag-node))))
+
+(defn links-on-page
+  [page-src]
+  (let [processed-pg (html->xml-doc page-src)
+        anchors      ($x ".//a" processed-pg)]
+    (map #(-> % :attrs :href) anchors)))
+
+(defn extract-in-domain-links
+  [body url-ds blacklist]
+  (let [ls (links-on-page body)]
+    (filter
+     (fn [x]
+       (not (some #{x} blacklist)))
+     (filter
+      identity
+      (filter
+       #(= (-> % uri/host)
+           (uri/host
+            (:url url-ds)))
+       (filter
+        identity
+        (map #(try (uri/resolve-uri (:url url-ds) %)
+                   (catch Exception e nil)) ls)))))))
+
+(defn fetch-random-corpus
+  [start-url]
+  (random-sample [{:url start-url}]
+                 (set [])
+                 extract-in-domain-links
+                 #(<= 100 %)
+                 {}))
