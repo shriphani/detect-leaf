12f7c163adbcfd1c14d1864c881ecb1253c6bba0
shriphani <spalakod@cs.cmu.edu>
2014-04-09 21:51:26 -0400

    added phpbb links

--- src/detect_leaf/corpus.clj
+++ src/detect_leaf/corpus.clj
@@ -39,40 +39,40 @@
    ["davidhorman" "http://forum.doom9.org/member.php?u=47770" false]
    ["SeeMoreDigital" "http://forum.doom9.org/member.php?u=35156" false]
 
-   ;; phpbb
-   []
-   []
-   []
-   []
-   []
-   []
-   []
-   []
-   []
+   ;; PHPbb official - phpbb
+   ["Announcements" "https://www.phpbb.com/community/viewforum.php?f=14" false]
+   ["Area 51 Development Board" "https://www.phpbb.com/community/viewforum.php?f=416" false]
+   ["3.0.x Support Forum" "https://www.phpbb.com/community/viewforum.php?f=46" false]
+   ["Preventing Spam in phpBB3" "https://www.phpbb.com/community/viewtopic.php?f=46&t=2122696" true]
+   ["System panel says version 3.0.12 but that I need to update" "https://www.phpbb.com/community/viewtopic.php?f=46&t=2236826" true]
+   ["ACP .MODS tab" "https://www.phpbb.com/community/viewtopic.php?f=46&t=2236906" true]
+   ["DV1" "https://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=1291506" false]
+   ["baconbuttyman" "https://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=1351099" false]
+   ["mrix2000" "https://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=286205" false]
 
 
-   ;; phpbb
-   []
-   []
-   []
-   []
-   []
-   []
-   []
-   []
-   []
+   ;; Psyonix - phpbb
+   ["Official Messages" "http://psyonix.com/forum/viewforum.php?f=19" false]
+   ["General Discussion" "http://psyonix.com/forum/viewforum.php?f=2" false]
+   ["Tournaments and Challenges" "http://psyonix.com/forum/viewforum.php?f=16" false]
+   ["Battle-Cars 2" "http://psyonix.com/forum/viewtopic.php?f=19&t=9278" true]
+   ["SARPBC For PS Plus (EU version)" "http://psyonix.com/forum/viewtopic.php?f=19&t=7398" true]
+   ["Psyonix Merchandise available for purchase now!" "http://psyonix.com/forum/viewtopic.php?f=19&t=6176" true]
+   ["Psyonix-Dave" "http://psyonix.com/forum/memberlist.php?mode=viewprofile&u=60" false]
+   ["Fysho" "http://psyonix.com/forum/memberlist.php?mode=viewprofile&u=503" false]
+   ["RAHTRICKRAH" "http://psyonix.com/forum/memberlist.php?mode=viewprofile&u=5369" false]
 
 
-   ;; phpbb
-   []
-   []
-   []
-   []
-   []
-   []
-   []
-   []
-   []
+   ;; Mx-Toolbox - phpbb
+   ["General Feedback" "http://community.mxtoolbox.com/forums/viewforum.php?f=5" false]
+   ["Ask MXToolbox" "http://community.mxtoolbox.com/forums/viewforum.php?f=6" false]
+   ["FlexBox Hosted Email" "http://community.mxtoolbox.com/forums/viewforum.php?f=7" false]
+   ["Reverse DNS FAILED! This is a problem." "http://community.mxtoolbox.com/forums/viewtopic.php?f=5&t=13303" true]
+   ["hi some one harrasing my daughter my daughter" "http://community.mxtoolbox.com/forums/viewtopic.php?f=5&t=1890634" true]
+   ["Please help me" "http://community.mxtoolbox.com/forums/viewtopic.php?f=5&t=1890582" true]
+   ["vishnuamz" "http://community.mxtoolbox.com/forums/memberlist.php?mode=viewprofile&u=3179" false]
+   ["pazza98" "http://community.mxtoolbox.com/forums/memberlist.php?mode=viewprofile&u=3082" false]
+   ["MichaelR" "http://community.mxtoolbox.com/forums/memberlist.php?mode=viewprofile&u=2970" false]
 
 
    ;; ipboard
