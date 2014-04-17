93d59ce6aeb8a556b319120b33d3679f0f6ce6ec
shriphani <spalakod@cs.cmu.edu>
2014-04-17 15:14:47 -0400

    added best performing model + classifier + train and test data

--- src/detect_leaf/train_svm.clj
+++ src/detect_leaf/train_svm.clj
@@ -26,14 +26,30 @@
 
 (defn evaluate
   [model-file test-file]
-  (let [model (read-model model-file)
-        data  (read-dataset test-file)]
-    (reduce
-     (fn [acc [label pred]]
-       (merge-with + acc (if (= label pred) {:success 1} {:fail 1})))
-     {}
-     (map
-      (fn [[label pt]]
-        (println label (int (predict model pt)))
-        [label (int (predict model pt))])
-      data))))
+  (let [{s :success f :fail}
+        (let [model (read-model model-file)
+              data  (read-dataset test-file)]
+          (reduce
+           (fn [acc [label pred]]
+             (merge-with + acc (if (= label pred) {:success 1} {:fail 1})))
+           {}
+           (map
+            (fn [[label pt]]
+              [label (int (predict model pt))])
+            data)))]
+    {:success s
+     :fail f
+     :accuracy (double (/ s (+ s f)))}))
+
+(defn train-and-test
+  [model-prefix]
+  (let [train-file (str model-prefix ".train.libsvm")
+        test-file  (str model-prefix ".test.libsvm")]
+    (do (build-train-svm train-file)
+        (build-test-svm test-file)
+        (let [model (train-svm train-file)
+              train-accuracy (evaluate model train-file)
+              test-accuracy  (evaluate model test-file)]
+          {:model-file model
+           :train-accuracy train-accuracy
+           :test-accuracy test-accuracy}))))
