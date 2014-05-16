47b0c366d5334b45666781574eb698ab83b66b28
shriphani <spalakod@cs.cmu.edu>
2014-05-15 20:07:21 -0400

    updated htmlcleaner

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[clj-http "0.9.0"]
                  [clj-logging-config "1.9.7"]
                  [clj-ml "0.0.3-SNAPSHOT"]
-                 [com.github.kyleburton/clj-xpath "1.4.2"]
+                 ;[com.github.kyleburton/clj-xpath "1.4.2"]
                  [enlive "1.1.5"]
                  [log4j/log4j "1.2.16"
                   :exclusions
@@ -14,13 +14,13 @@
                    javax.jms/jms
                    com.sun.jdmk/jmxtools
                    com.sun.jmx/jmxri]]
-                 [net.sourceforge.htmlcleaner/htmlcleaner "2.6"]
-                 [org.apache.commons/commons-lang3 "3.1"]
+                 [net.sourceforge.htmlcleaner/htmlcleaner "2.8"]
+                 ;[org.apache.commons/commons-lang3 "3.1"]
                  [com.github.rholder/snowball-stemmer "1.3.0.581.1"]
-                 [org.bovinegenius/exploding-fish "0.3.3"]
+                 ;[org.bovinegenius/exploding-fish "0.3.3"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/clojure "1.5.1"]
-                 [structural_similarity/structural_similarity "0.1.0-SNAPSHOT"]
+                 ;[structural_similarity/structural_similarity "0.1.0-SNAPSHOT"]
                  [svm-clj "0.1.3"]]
   :plugins [[lein-gorilla "0.2.0"]])
